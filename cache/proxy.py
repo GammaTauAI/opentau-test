5f4b205854c3ed7dd9ed02de4eaea4c07ff69d25
elleven11 <federico.cassano@federico.codes>
2022-09-26 14:09:09 -0400

    add delete

--- cache/proxy.py
+++ cache/proxy.py
@@ -8,14 +8,28 @@ import sqlite3
 import requests
 import hashlib
 
-# HTTPRequestHandler class
+db = sqlite3.connect('cache.db')
+cursor = db.cursor()
 
 
 class HTTPServer_RequestHandler(http.server.BaseHTTPRequestHandler):
-    def __init__(self, request, client_address, server):
-        self.db = sqlite3.connect('cache.db')
-        self.cursor = self.db.cursor()
-        super().__init__(request, client_address, server)
+    def do_DELETE(self):  # way of clearing a specific cache entry
+        # get request body
+        content_length = int(self.headers['Content-Length'])
+        body = self.rfile.read(content_length)
+        jsonData = body.decode('utf-8')
+        sha256 = hashlib.sha256(jsonData.encode('utf-8')).hexdigest()
+
+        # delete from cache
+        cursor.execute('DELETE FROM cache WHERE sha256=?', (sha256,))
+        db.commit()
+
+        # send response to client
+        self.send_response(200)
+        self.send_header('Content-Type', 'application/json')
+        self.end_headers()
+        self.wfile.write('{"success": true}'.encode('utf-8'))
+        return
 
     def do_POST(self):
         # get request body
@@ -25,9 +39,10 @@ class HTTPServer_RequestHandler(http.server.BaseHTTPRequestHandler):
         sha256 = hashlib.sha256(jsonData.encode('utf-8')).hexdigest()
 
         # check if request is in cache
-        self.cursor.execute('SELECT * FROM cache WHERE sha256=?', (sha256,))
-        result = self.cursor.fetchone()
+        cursor.execute('SELECT * FROM cache WHERE sha256=?', (sha256,))
+        result = cursor.fetchone()
         if result is None:
+            print('cache miss')
             # get auth token
             auth_token = self.headers['Authorization']
 
@@ -40,9 +55,9 @@ class HTTPServer_RequestHandler(http.server.BaseHTTPRequestHandler):
             )
 
             # add to cache
-            self.cursor.execute(
+            cursor.execute(
                 'INSERT INTO cache VALUES (?, ?, ?)', (sha256, jsonData, response.text))
-            self.db.commit()
+            db.commit()
 
             # send response to client
             self.send_response(response.status_code)
@@ -51,14 +66,19 @@ class HTTPServer_RequestHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(response.text.encode('utf-8'))
         else:
             # send response from cache to client
+            print('cache hit')
             self.send_response(200)
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
-            self.wfile.write(result[1].encode('utf-8'))
+            self.wfile.write(result[2].encode('utf-8'))
         return
 
 
 def run():
+    cursor.execute(
+        'CREATE TABLE IF NOT EXISTS cache (sha256 text PRIMARY KEY, request text, response text)')
+    db.commit()
+
     # Create an object of the above class
     handler_object = HTTPServer_RequestHandler
 
