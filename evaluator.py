8a218b308f6787601ed8a3927bcc614f6b34abfe
elleven11 <federico.cassano@federico.codes>
2022-09-27 15:08:45 -0400

    eval

--- evaluator.py
+++ evaluator.py
@@ -4,10 +4,12 @@ import sys
 import csv
 import json
 import subprocess
+from typing import Dict
 
 
 if len(sys.argv) != 5:
-    print(f"USAGE: {sys.argv[0]} test-file-dir/ perm.json client.bin out/results.csv")
+    print(
+        f"USAGE: {sys.argv[0]} test-file-dir/ perm.json client.bin out/results.csv")
     sys.exit(1)
 
 
@@ -25,16 +27,17 @@ if _CODEX_TOKEN is None:
 
 class Permutation:
     def __init__(self, n: int, r: int, temp: float, strategy: str) -> None:
-        self.n = n 
+        self.n = n
         self.r = r
         self.temp = temp
         self.strategy = strategy
-    
-    @staticmethod
-    def deserialize(s: str):
-        d = json.loads(s) 
-        return Permutation(**d)
 
+    @staticmethod
+    def deserialize(s: Dict):
+        return Permutation(**s)
+
+    def __repr__(self):
+        return f"Permutation(n={self.n}, r={self.r}, temp={self.temp}, strategy={self.strategy})"
 
 
 def main() -> None:
@@ -44,25 +47,34 @@ def main() -> None:
 
     with open(_PERM_JSON) as f:
         perms = [Permutation.deserialize(i) for i in json.load(f)]
-    
+
     with open(_RESULTS_PATH, 'w') as write_file:
-        write_file.write(_CSV_HEADER)         
+        write_file.write(_CSV_HEADER)
         for f in files:
             lang = f.split(".")[-1]
             for p in perms:
+                print(f"running {f} with {p.__repr__()}")
                 cmd = f"{_CLIENT_PATH} --token {_CODEX_TOKEN} --file {f} --output /tmp/deleteme --lang {lang} --retries {p.r} --n {p.n} --temp {p.temp} --strategy {p.strategy}"
                 cmd_ = cmd.split()
-                sp = subprocess.Popen(cmd_, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                sp = subprocess.Popen(
+                    cmd_, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                 out, err = sp.communicate()
+
                 if 'Rate limited' in err.decode("utf-8"):
+                    print(f"got rate limited. sleeping")
                     time.sleep(70)
-                    sp = subprocess.Popen(cmd_, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    sp = subprocess.Popen(
+                        cmd_, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                     out, err = sp.communicate()
                     if 'Rate limited' in err.decode("utf-8"):
+                        print(f"got rate limited again!!!!")
                         time.sleep(70)
 
                 status = sp.returncode
-
+                if status == 0:
+                    comp = out.decode("utf-8").split("completed:\n")[-1]
+                    print(f"got completion: {comp}")
+                print(f"status: {status}")
 
                 row = f'{f},{lang},{p.strategy},{p.r},{p.n},{p.temp},{status}\n'
                 write_file.write(row)
@@ -70,7 +82,6 @@ def main() -> None:
 
 if __name__ == '__main__':
     main()
-
 
 
 # 1. get the path of the files to test from argv[1]
@@ -85,4 +96,3 @@ if __name__ == '__main__':
 #    - you know if it typechecked, if there is a completed file at the output
 
 # 6. output csv file nguage,strategy,retries,num_comp,result
-
